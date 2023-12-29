4725b5a2e0b2417bbcb05001c821d2b1a1422bb7
gigis-lab <churn82@gmail.com>
2023-12-29 22:09:05 +0900

    [#2]Fix: upload->diskStorage
    uuid를 이용해서 이미지별 고유 아이디를 생성하려고 하는데 그럴려면 이미지의 원래 파일 이름이 아닌 내가 설정한 이름으로 저장하도록 해야됩니다.  multer.diskStorage를 이용하면 파일 저장 과정을 내가 제어할 수 있습니다.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const multer = require("multer");
-const upload = multer({ dest: "uploads" });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
 
 const app = express();
 const PORT = 5000;
