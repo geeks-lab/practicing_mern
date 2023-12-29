63f5eaeaade0392ca49cfd7fbe7b108588d8949f
gigis-lab <churn82@gmail.com>
2023-12-30 00:44:26 +0900

    [#2]Feat: 이미지 파일들 외부로 노출 시켜주기
    `app.use("/uploads", express.static("uploads"));`
    코드를 추가하여 /uploads/파일명.파일타입
    으로 이렇게 외부에서 uploads 폴더에 접근 가능하게 했습니다.

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const upload = multer({ storage });
 const app = express();
 const PORT = 5000;
 
+app.use("/uploads", express.static("uploads"));
+
 app.post("/upload", upload.single("imageTest"), (req, res) => {
   console.log(req.file);
   res.json(req.file);
