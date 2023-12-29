d55ade90b6353f569a8dd34cf216adf0ad87ecc7
gigis-lab <churn82@gmail.com>
2023-12-30 02:41:20 +0900

    [#2]Feat: 이미지 저장하기 전 필터 걸어주기
    const upload = multer({ storage }); 이 부분에 fileFilter 옵션을 넣어서 어떤 파일들을 accept할 건지 정할 수 있습니다.
    file의 size를 limits 옵션을 사용해서 조건을 걸 수도 있습니다.

--- server/server.js
+++ server/server.js
@@ -8,7 +8,16 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) =>
     cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
 });
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (["image/jpeg", "image/png"].includes(file.mimetype)) cb(null, true);
+    else cb(new Error("invalid file type."), false);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
 
 const app = express();
 const PORT = 5000;
