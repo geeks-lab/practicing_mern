6fd8532748fcca84345c10580d306bbb2dde19d2
gigis-lab <churn82@gmail.com>
2023-12-29 21:48:49 +0900

    [#2]Feat: multer 이용해서 이미지 저장하기

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "uploads" });
+
+const app = express();
+const PORT = 5000;
+
+app.post("/upload", upload.single("imageTest"), (req, res) => {
+  console.log(req.file);
+  res.json(req.file);
+});
+
+app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
