d65c124742e4f045bd67fdffa84bf37631913f79
gigis-lab <churn82@gmail.com>
2023-12-31 00:09:46 +0900

    [#9]Feat: useState로 이미지 파일 데이터 관리하기
    useState를 사용해서 아무런 파일이 들어오지 않았을 때 파일을 업로드하라는 메세지를 띄우고 파일이 업로드 되면 파일의 이름을 띄워줍니다.

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("upload the image file");
   return (
     <form>
-      <label htmlfor="image">사진</label>
-      <input id="image" type="file" />
+      <label htmlFor="image">{fileName}</label>
+      <input
+        id="image"
+        type="file"
+        onChange={(event) => {
+          const imageFile = event.target.files[0];
+          setFile(imageFile);
+          setFileName(imageFile.name);
+        }}
+      />
       <button type="submit">제출</button>
     </form>
   );
 };
 
 export default UploadForm;
-// form component 만듦
