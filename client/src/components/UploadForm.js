fbc0163fa8b6a464df88c9390274117c9c824f03
gigis-lab <churn82@gmail.com>
2023-12-31 03:20:51 +0900

    [#9]Feat: axios 이용해서 백엔드로 이미지 전송하기
    package.json에 proxy를 추가한 이유는 CORS 문제가 생겼었는데 현재로써 가장 간편하게 해결하기 위함입니다.

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("upload the image file");
+
+  const imageSelectHandler = (event) => {
+    const imageFile = event.target.files[0];
+    setFile(imageFile);
+    setFileName(imageFile.name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      console.log({ res });
+      alert("success!");
+    } catch (err) {
+      alert("fail!");
+      console.error(err);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label htmlFor="image">{fileName}</label>
-      <input
-        id="image"
-        type="file"
-        onChange={(event) => {
-          const imageFile = event.target.files[0];
-          setFile(imageFile);
-          setFileName(imageFile.name);
-        }}
-      />
+      <input id="image" type="file" onChange={imageSelectHandler} />
       <button type="submit">제출</button>
     </form>
   );
