32433cd868867e0556458d9f9e07d39af1fb8772
gigis-lab <churn82@gmail.com>
2024-01-01 15:27:56 +0900

    [#12]Env: mongodb atlas 설정 및 Node.js로 연결하기
    server side에서 npm i mongoose를 해줘야 합니다.
    인덱스를 사용할 경우를 위해 useCreateIndex 옵션을 true로 설정하여 index값이 안들어가 있는 경우에 자동으로 들어갈 수 있게 하였습니다.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "./uploads"),
@@ -22,8 +23,30 @@ const upload = multer({
 const app = express();
 const PORT = 5000;
 
-app.use("/uploads", express.static("uploads"));
+mongoose
+  .connect(
+    "mongodb+srv://admin:irq6GQYpysCMKbtz@mongocluster.1bagdmk.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true, // incase of using Index. If this is true, it creats an index for the ones that doesn't have.
+    }
+  )
+  .then(() => {
+    console.log("MongoDB Connected.");
+    app.use("/uploads", express.static("uploads"));
 
-app.post("/upload", upload.single("image"), (req, res) => res.json(req.file));
+    app.post("/upload", upload.single("image"), (req, res) =>
+      res.json(req.file)
+    );
 
-app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
+    app.listen(PORT, () =>
+      console.log("Express server listening on PORT " + PORT)
+    );
+  })
+  .catch((err) => console.log(err));
+
+// Changed the location from here to after then, because if the database is not connected, the server should not be started.
+// app.use("/uploads", express.static("uploads"));
+
+// app.post("/upload", upload.single("image"), (req, res) => res.json(req.file));
+
+// app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
