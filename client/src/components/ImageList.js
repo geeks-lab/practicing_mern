4a5db1e9b4a276e0cd14592b7142dbc382cb3518
geeks-lab <churn82@gmail.com>
2024-01-01 19:49:49 +0900

    [#14]Feat: useEffect로 이미지 데이터 불러오기
    `Each child in a list should have a unique "key" prop.` 해당 에러는 다음 강의에서 진행할 부분 같기에 일단 커밋합니다.

---
+++ client/src/components/ImageList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ImageList = () => {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/images") // localhost:5000 is not necessary at here thanks to the proxy
+      .then((result) => setImages(result.data))
+      .catch((err) => console.error(err));
+  }, []);
+  const imgList = images.map((image) => (
+    <img
+      style={{ width: "100%" }}
+      src={`http://localhost:5000/uploads/${image.key}`}
+    />
+  ));
+  return (
+    <div>
+      <h3>Image List</h3>
+      {imgList}
+    </div>
+  );
+};
+
+export default ImageList;
