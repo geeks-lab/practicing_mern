b3f814696625fb1f68ff06c1a4e9e2690258e9ca
gigis-lab <churn82@gmail.com>
2024-01-05 17:47:27 +0900

    [#18]Feat: toolbar 만들기
    server 이 전 커밋부터 계속 아래와 같은 에러 뜨고 있습니다. TypeError: Cannot destructure property 'sessionid' of 'req.headers' as it is undefined.

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
 import { Routes, Route } from "react-router-dom";
+import ToolBar from "./components/ToolBar";
 
 const App = () => {
   return (
     <div style={{ maxwidth: 600, margin: "auto" }}>
       <ToastContainer />
+      <ToolBar />
       <Routes>
         <Route path="/auth/register" exact element={<RegisterPage />} />
         <Route path="/auth/login" exact element={<LoginPage />} />
