1f3b63d4b309e8d0e3366ac3678d28f2f80fb041
gigis-lab <churn82@gmail.com>
2024-01-05 18:05:35 +0900

    [#18]Feat: 회원가입 form 만들기

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import CustomInput from "../components/CustomInput";
 
 const RegisterPage = () => {
-  return <h3>Register</h3>;
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, SetPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  return (
+    <div
+      style={{
+        marginTop: 100,
+        maxWidth: 350,
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <h3>회원가입</h3>
+      <form>
+        <CustomInput label="이름" value={name} setValue={setName} />
+        <CustomInput label="회원ID" value={username} setValue={setUsername} />
+        <CustomInput
+          label="비밀번호"
+          value={password}
+          setValue={SetPassword}
+          type="password"
+        />
+        <CustomInput
+          label="비밀번호 확인"
+          value={passwordCheck}
+          setValue={setPasswordCheck}
+          type="password"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
