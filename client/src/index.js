1842f185e181d03051f1ebbd2e9fda9e78693861
gigis-lab <churn82@gmail.com>
2024-01-05 17:32:13 +0900

    [#18]Feat: react-router-dom 적용하기
    페이지 분리(app-> main, register, login)

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { ImageProvider } from "./context/ImageContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ImageProvider>
-      <App />
-    </ImageProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <ImageProvider>
+        <App />
+      </ImageProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
