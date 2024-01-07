a363b5c2b666da040fddff34cf4a70bbfe610283
gigis-lab <churn82@gmail.com>
2024-01-08 06:40:33 +0900

    [#22]Feat: 이미지 상세 페이지 구현하기
    새로고침 하면 이미지 로딩되기 전에 불러와서 생기는 에러 때문에 `if (!image) return <h3>Loading...</h3>;`를 추가해주었습니다.

--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { ImageContext } from "../context/ImageContext";
 import { AuthContext } from "../context/AuthContext";
 import "./ImageList.css";
@@ -7,11 +8,9 @@ const ImageList = () => {
   const { images, myImages, isPublic, setIsPublic } = useContext(ImageContext);
   const [me] = useContext(AuthContext);
   const imgList = (isPublic ? images : myImages).map((image) => (
-    <img
-      alt=""
-      key={image.key}
-      src={`http://localhost:5000/uploads/${image.key}`}
-    />
+    <Link key={image.key} to={`/images/${image._id}`}>
+      <img alt="" src={`http://localhost:5000/uploads/${image.key}`} />
+    </Link>
   ));
   return (
     <div>
---
+++ client/src/pages/ImagePage.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { useParams } from "react-router";
+import { ImageContext } from "../context/ImageContext";
+
+const ImagePage = () => {
+  const { imageId } = useParams();
+  const { images, myImages } = useContext(ImageContext);
+  const image =
+    images.find((image) => image._id === imageId) ||
+    myImages.find((image) => image._id === imageId);
+  if (!image) return <h3>Loading...</h3>;
+  return (
+    <div>
+      <h3>Image Page - {imageId}</h3>
+      <img
+        style={{ width: "100%" }}
+        alt={imageId}
+        src={`http://localhost:5000/uploads/${image.key}`}
+      />
+    </div>
+  );
+};
+
+export default ImagePage;
